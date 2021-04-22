b10ebd64bc54ebedf805184aee3f7441a4bd291a
karinaornelas24 <75212858+karinaornelas24@users.noreply.github.com>
2021-04-22 18:01:10 +0200

    Added Search Engine

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <div className="container">
+        <header className="App-header"></header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer>
+          <small>Coded by Ana Karina Ornelas</small>
+        </footer>
+      </div>
     </div>
   );
 }
