bd60bc62a7d3fd37e52f3b4006231a147816ebc3
karinaornelas24 <75212858+karinaornelas24@users.noreply.github.com>
2021-04-24 12:20:30 +0200

    Updated keyword

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <div className="container">
         <header className="App-header"></header>
         <main>
-          <Dictionary defaultKeyword="hello" />
+          <Dictionary defaultKeyword="yoga" />
         </main>
         <footer>
           <small>Coded by Ana Karina Ornelas </small>open-sourced on{" "}
