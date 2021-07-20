03379727b9a29777abfd03e43f27152a8b110ffe
karinaornelas24 <75212858+karinaornelas24@users.noreply.github.com>
2021-07-20 11:53:03 +0200

    Updated

--- src/Meaning.js
+++ src/Meaning.js
@@ -5,7 +5,7 @@ import Synonyms from "./Synonyms";
 export default function Meaning(props) {
   return (
     <div className="Meaning">
-      <h3>{props.meaning.partOfSpeech}</h3>
+      <h3 class="multicolortext">{props.meaning.partOfSpeech}</h3>
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
